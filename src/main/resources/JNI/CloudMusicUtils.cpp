3050803f5778f38e507c35160285ea15865d9cb1
MyLovePoppet <2393800169@qq.com>
2021-02-01 11:41:09 +0800

    fix: JNI代码重构

--- src/main/resources/JNI/CloudMusicUtils.cpp
+++ src/main/resources/JNI/CloudMusicUtils.cpp
@@ -1,96 +1,18 @@
 #include "pch.h"
 #include "CloudMusicUtils.h"
-#include <iostream>
-#include <Psapi.h>
-HWND hwnd = NULL;
-DWORD processID = NULL;
-HANDLE processHandle = NULL;
-DWORD_PTR offsetAddress = NULL;
-/*****************************************************************
- *wchar_t* ----->jstring
- *******************************************************************/
-jstring w2js(JNIEnv* env, wchar_t* str)
-{
-	size_t len = wcslen(str);
-	jchar* str2 = (jchar*)malloc(sizeof(jchar) * (len + 1));
-	size_t i;
-	for (i = 0; i < len; i++)
-		str2[i] = str[i];
-	str2[len] = 0;
-	jstring js = env->NewString(str2, len);
-	free(str2);
-	return js;
-}
-
+#include "JNIUtils.h"
+#include "CloudMusicImpl.h"
+//auto_ptr
+std::shared_ptr<IBGMProtocol> cloudMusicUtil = std::make_shared<CloudMusicImpl>();
 /*
  * Class:     CloudMusicUtils
  * Method:    initNativeDll
  * Signature: ()Z
  */
-JNIEXPORT jboolean JNICALL Java_CloudMusicUtils_initNativeDll
+inline JNIEXPORT jboolean JNICALL Java_CloudMusicUtils_initNativeDll
 (JNIEnv*, jclass)
 {
-	hwnd = FindWindow(L"OrpheusBrowserHost", NULL);
-	if (hwnd == NULL)
-	{
-		return JNI_FALSE;
-	}
-
-	GetWindowThreadProcessId(hwnd, &processID);
-	if (processID == NULL)
-	{
-		return JNI_FALSE;
-	}
-
-	DWORD_PTR baseAddress = 0;
-	processHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, processID);
-	HMODULE* moduleArray;
-	LPBYTE moduleArrayBytes;
-	DWORD bytesRequired;
-	if (processHandle)
-	{
-		if (EnumProcessModules(processHandle, NULL, 0, &bytesRequired))
-		{
-			if (bytesRequired)
-			{
-				moduleArrayBytes = (LPBYTE)LocalAlloc(LPTR, bytesRequired);
-				if (moduleArrayBytes)
-				{
-					unsigned int moduleCount;
-					moduleCount = bytesRequired / sizeof(HMODULE);
-					moduleArray = (HMODULE*)moduleArrayBytes;
-					if (EnumProcessModules(processHandle, moduleArray, bytesRequired, &bytesRequired))
-					{
-						wchar_t moduleName[256];
-						for (unsigned int i = 0; i < moduleCount; i++)
-						{
-							GetModuleBaseName(processHandle, moduleArray[i], moduleName, 256);
-							if (std::wcscmp(moduleName, L"cloudmusic.dll") == 0)
-							{
-								baseAddress = (DWORD_PTR)moduleArray[i];
-								break;
-							}
-						}
-					}
-					LocalFree(moduleArrayBytes);
-				}
-			}
-		}
-	}
-	else
-	{
-		return JNI_FALSE;
-	}
-
-	if (baseAddress != 0)
-	{
-		offsetAddress = 0x8669C8 + baseAddress;
-	}
-	else
-	{
-		return JNI_FALSE;
-	}
-	return JNI_TRUE;
+	return cloudMusicUtil->initNativeProtocol() ? JNI_TRUE : JNI_FALSE;
 }
 
 /*
@@ -98,12 +20,11 @@ JNIEXPORT jboolean JNICALL Java_CloudMusicUtils_initNativeDll
  * Method:    getCloudMusicTitle
  * Signature: (J)Ljava/lang/String;
  */
-JNIEXPORT jstring JNICALL Java_CloudMusicUtils_getCloudMusicTitle
+inline JNIEXPORT jstring JNICALL Java_CloudMusicUtils_getCloudMusicTitle
 (JNIEnv* env, jclass)
 {
-	wchar_t titleStr[256];
-	GetWindowText(hwnd, titleStr, 256);
-	return w2js(env, titleStr);
+	std::wstring title = cloudMusicUtil->getTitle();
+	return JNIUtils::w2js(env, title);
 }
 
 /*
@@ -111,25 +32,18 @@ JNIEXPORT jstring JNICALL Java_CloudMusicUtils_getCloudMusicTitle
  * Method:    getCurrentPosition
  * Signature: (JI)D
  */
-JNIEXPORT jdouble JNICALL Java_CloudMusicUtils_getCurrentPosition
+inline JNIEXPORT jdouble JNICALL Java_CloudMusicUtils_getCurrentPosition
 (JNIEnv*, jclass)
 {
-	//HANDLE processHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, processID);
-	double position;
-	SIZE_T outNum;
-	ReadProcessMemory(processHandle, (LPCVOID)offsetAddress, (LPVOID)(&position), 8, &outNum);
-	return position;
+	return cloudMusicUtil->getPosition();
 }
-
-/*
 /*
  * Class:     CloudMusicUtils
  * Method:    nativeGc
  * Signature: ()V
  */
-JNIEXPORT void JNICALL Java_CloudMusicUtils_nativeGc
+inline JNIEXPORT void JNICALL Java_CloudMusicUtils_nativeGc
 (JNIEnv*, jclass)
 {
-	if (processHandle != NULL)
-		CloseHandle(processHandle);
+	cloudMusicUtil->nativeGc();
 }
